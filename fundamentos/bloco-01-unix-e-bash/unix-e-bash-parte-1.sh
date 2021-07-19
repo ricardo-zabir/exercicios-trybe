ab43121060e2bdad30ea827a6bccbd75caf877d0
ricardo-zabir <ricardofonseca.zabir@hotmail.com>
2021-07-19 19:30:18 -0300

    Resolução dos exercícios da primeira parte do primeiro dia do conteúdo Unix e Bash

---
+++ fundamentos/bloco-01-unix-e-bash/unix-e-bash-parte-1.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Ex. 1
+mkdir unix_tests
+
+# Ex. 2
+touch trybe.txt
+
+# Ex. 3
+cp trybe.txt trybe_backup.txt
+
+# Ex. 4
+mv trybe.txt trybe_novo_nome.txt
+
+# Ex. 5
+mkdir backup
+
+# Ex. 6
+mv trybe_backup.txt backup
+
+# Ex. 7
+mkdir backup2
+
+# Ex. 8
+mv backup/trybe_backup.txt backup2
+
+# Ex. 9
+rmdir backup
+
+# Ex. 10
+mv backup2 backup
+
+# Ex. 11
+pwd
+ls *
+
+# Ex. 12
+rm - ra backup
+
+# Ex. 13
+clear
+
+# Ex . 14
+head -5 skills.txt
+
+# Ex. 15
+tail -4 skills.txt
+
+# Ex. 16
+rm *txt
