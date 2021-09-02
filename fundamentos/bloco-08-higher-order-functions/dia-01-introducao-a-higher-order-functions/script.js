aa524887bacd728b28a5dfcf53ed63e321fbe978
ricardo-zabir <ricardofonseca.zabir@hotmail.com>
2021-09-02 14:57:42 -0300

    Exercícios de fixação dia 8.1

---
+++ fundamentos/bloco-08-higher-order-functions/dia-01-introducao-a-higher-order-functions/script.js
@@ -0,0 +1,6 @@
+const wakeUpCall = () => 'Acordando';
+const coffeTime = () => 'Bora tomar café';
+const sleepTime = () => 'Partiu dormir';
+
+const doingThings = (func) => func();
+console.log(doingThings(wakeUpCall)); 
